6e42a7cce5387b0205743997165162e8a69e7b1a
banderui1993 <hebth_fy1993@163.com>
2015-10-26 21:20:31 +0800

    测试
    输出不同

---
+++ success/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  success
+//
+//  Created by qianfeng on 15/10/25.
+//  Copyright (c) 2015年 FY. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"测试!");
+        
+    }
+    return 0;
+}
+
